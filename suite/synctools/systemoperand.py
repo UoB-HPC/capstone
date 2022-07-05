f1c1d949d3c6d65b35df4da89e532382b190c129
Finn Wilkinson <finnwilkinson5@gmail.com>
2022-07-05 12:33:03 +0100

    Updated systemoperand.py to extract the new SME SVCRsList structure and generate lookup function.

--- suite/synctools/systemoperand.py
+++ suite/synctools/systemoperand.py
@@ -921,6 +921,70 @@ print_line("""
 }
 """)
 
+# extract SVCRsList
+count = 0
+c = 0
+for line in lines:
+    line = line.rstrip()
+
+    if len(line.strip()) == 0:
+        continue
+
+    if line.strip() == 'constexpr SVCR SVCRsList[] = {':
+        count += 1
+        print_line('static const SVCR SVCRsList[] = {')
+        continue
+
+    line = line.strip()
+    if count == 1:
+        if line == '};':
+            # done with first enum
+            print_line('};\n')
+            break
+        else:
+            # skip pseudo instructions
+            line = line.replace('::', '_')
+            #line = line.replace('{}', '{ 0 }')
+            line = line.replace('{}', '')
+            tmp = line.split(',')
+            print_line("  %s, %s }, // %u" %(tmp[0].lower(), tmp[1], c))
+            c += 1
+
+# lookupSVCRByEncoding
+count = 0
+for line in lines:
+    line = line.rstrip()
+
+    if len(line.strip()) == 0:
+        continue
+
+    if 'lookupSVCRByEncoding' in line and '{' in line:
+        count += 1
+        print_line('const SVCR *lookupSVCRByEncoding(uint8_t Encoding)\n{')
+        print_line('  unsigned int i;')
+        continue
+
+    if count == 1 and 'IndexType Index[] = {' in line:
+        count += 1
+
+    if count == 2:
+        if line.strip() == '};':
+            # done with array, or this function?
+            print_line(line)
+            break
+        else:
+            # enum items
+            print_line(line)
+
+print_line("""
+  i = binsearch_IndexTypeEncoding(Index, ARR_SIZE(Index), Encoding);
+  if (i == -1)
+    return NULL;
+  else
+    return &SVCRsList[Index[i].index];
+}
+""")
+
 
 # extract ExactFPImmsList
 count = 0
